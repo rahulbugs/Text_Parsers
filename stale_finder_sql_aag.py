5b97ba1e589b8fb9d370792933ddf0f509a514c4
Rahul R <rahulravir1992@gmail.com>
2024-05-12 11:30:13 +0800

    Update stale_finder_sql_aag.py

--- stale_finder_sql_aag.py
+++ stale_finder_sql_aag.py
@@ -1,23 +1,35 @@
-#!/usr/bin/python3
-
 import os
 import re
+import sys
 
 # Function to print in red
 def print_in_red(text):
     print("\033[91m{}\033[00m" .format(text))
 
-# Define a regular expression pattern to match IP addresses
-ip_address_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+# Check if the file path is provided as an argument
+if len(sys.argv) < 2:
+    print("Usage: python script.py <file_path>")
+    sys.exit(1)
 
-# Get the current directory
-current_directory = os.getcwd()
+# Get the file path from the command line arguments
+file_path = sys.argv[1]
+
+# Ensure that the file exists
+if not os.path.exists(file_path):
+    print(f"Error: File '{file_path}' not found.")
+    sys.exit(1)
+
+# Get the directory containing the file
+current_directory = os.path.dirname(file_path)
 
 # List all directories in the current directory
 directories = [d for d in os.listdir(current_directory) if os.path.isdir(os.path.join(current_directory, d))]
 
 # Initialize an empty array to store directories with IP addresses
 ip_directories = []
+
+# Define a regular expression pattern to match IP addresses
+ip_address_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
 
 # Filter directories that contain an IP address in their names
 for directory in directories:
@@ -51,6 +63,6 @@ for file in magneto_exec_files:
         for line_number, line in enumerate(f, start=1):
             # Check if both 'kStale' and 'Task id' are in the line
             if 'kStale' in line and 'Task id' in line:
-                print_in_red("Consider applying magneto_sql_enable_refresh_eh_before_full_inc_backup for incremental backups and magneto_sql_enable_refresh_eh_before_log_backup if the metadata changesaare too frequent")
+                print_in_red("Consider applying magneto_sql_enable_refresh_eh_before_full_inc_backup for incremental backups")
+                print_in_red("and setting magneto_sql_enable_refresh_eh_before_log_backup for log backups.")
                 break
-
